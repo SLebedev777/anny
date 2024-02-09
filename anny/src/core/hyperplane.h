d5077ce1fd6c82b7077519a301ab160bb9b63aad
SLebedev777 <naotlichnenko@gmail.com>
2024-02-09 19:10:52 +0300

    hyperplane + tests

---
+++ anny/src/core/hyperplane.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <cmath>
+#include "vec.h"
+#include "vec_view.h"
+#include "distance.h"
+
+
+namespace anny
+{
+
+	template <typename T>
+	struct Hyperplane
+	{
+		Hyperplane() = default;
+
+		explicit Hyperplane(const Vec<T>& _normal, T _intercept = T{ 0 })
+			: normal{ _normal }
+			, intercept{ _intercept }
+		{
+			assert(anny::is_l2_normalized(normal.view()));
+		}
+
+		Hyperplane(const Vec<T>& _normal, const Vec<T>& x0)
+			: normal{ _normal }
+			, intercept{ -anny::dot(_normal.view(), x0) }
+		{}
+		
+		T distance(VecView<T> v)
+		{
+			return std::fabs(anny::dot(normal.view(), v) + intercept);
+		}
+		
+		T distance(VecView<const T> v) const
+		{
+			return std::fabs(anny::dot(normal.view(), v) + intercept);
+		}
+		
+		bool side(VecView<T> v)
+		{
+			return (anny::dot(normal.view(), v) + intercept) >= 0.0;
+		}
+
+		bool side(VecView<const T> v) const
+		{
+			return (anny::dot(normal.view(), v) + intercept) >= 0.0;
+		}
+
+		size_t size() const
+		{
+			return normal.size();
+		}
+
+		Vec<T> normal;
+		T intercept{};
+	};
+
+
+	template <typename T>
+	anny::Hyperplane<T> make_orthogonal_hyperplane(size_t size, size_t normal_dim, T intercept = T{ 0.0 })
+	{
+		Vec<T> normal(size, T{ 0.0 });
+		normal[normal_dim] = T{ 1.0 };
+		return anny::Hyperplane<T>(normal, intercept);
+	}
+
+}
---
+++ anny/tests/HyperplaneTests.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "core/vec.h"
+#include "core/vec_view.h"
+#include "core/hyperplane.h"
+
+
+using namespace anny;
+
+TEST(HyperplaneTests, HyperplaneDefaultCtorTest)
+{
+    {
+        Hyperplane<float> h;
+
+        EXPECT_EQ(h.normal.size(), 0);
+        EXPECT_EQ(h.intercept, 0.0f);
+        Vec<float> vec_zero{};
+        EXPECT_EQ(h.distance(vec_zero.view()), 0);
+    }
+}
+
+TEST(HyperplaneTests, HyperplaneDistanceTest)
+{
+    {
+        // this vertical 2d plane has equation x - 5 = 0 for every y
+        double intercept = -5.0;
+        auto h = make_orthogonal_hyperplane<double>(2, 0, intercept);
+
+        double delta = 3.0;
+        Vec<double> point{ -intercept + delta, 3 };  // (8;3)
+
+        EXPECT_EQ(h.distance(point.view()), delta);
+    }
+
+    {
+        const float c = 12345.12345f;
+        Vec<float> n{ -c, c };  // 45 degrees diagonal 2d line
+        n = l2_normalize(n.view());
+
+        float intercept = -sqrt(2.0f);  // so the plane goes through points (-2; 0) and (0; 2)
+        Hyperplane<float> h(n, intercept);
+
+        Vec<float> point1{ -2.0f, 0.0f };
+        Vec<float> point2{ 0.0f, 2.0f };
+        Vec<float> point3{ 1.0f, 6.0f }; // 2 points equidistant from plane
+        Vec<float> point4{ 4.0f, 3.0f };
+
+        auto d1 = h.distance(point1.view());
+        auto d2 = h.distance(point2.view());
+        auto d3 = h.distance(point3.view());
+        auto d4 = h.distance(point4.view());
+
+        EXPECT_TRUE(anny::are_floats_equal(d1, 0.0f));
+        EXPECT_TRUE(anny::are_floats_equal(d2, 0.0f));
+        EXPECT_TRUE(anny::are_floats_equal(d3, d4));
+
+    }
+}
+
+TEST(HyperplaneTests, HyperplaneSideTest)
+{
+    {
+        const float c = 12345.12345f;
+        Vec<float> n{ -c, c };  // 45 degrees diagonal 2d line
+        n = l2_normalize(n.view());
+
+        float intercept = -sqrt(2.0f);  // so the plane goes through points (-2; 0) and (0; 2)
+        Hyperplane<float> h(n, intercept);
+
+        Vec<float> point3{ 1.0f, 6.0f }; // 2 points equidistant from plane
+        Vec<float> point4{ 4.0f, 3.0f };
+
+        EXPECT_TRUE(h.side(point3.view()));  // point above plane 
+        EXPECT_FALSE(h.side(point4.view())); // point below plane
+
+    }
+
+}
