6d87552d278ad49c9ff26850cce1ffacf0e942c8
SLebedev777 <naotlichnenko@gmail.com>
2024-08-12 02:10:05 +0300

    HNSW: uniform dataset test

--- anny/tests/HNSWTests.cpp
+++ anny/tests/HNSWTests.cpp
@@ -40,3 +40,31 @@ TEST(HNSWTests, HNSWBuildTestSimple)
 	
 }
 
+TEST(HNSWTests, HNSWTestRandomDatasetClusters)
+{
+	//GTEST_SKIP();
+
+	std::cout << "Generating dataset..." << std::endl;
+	auto data = anny::utils::make_uniform<double>(10000, 16, -100.0, 100.0);
+
+	std::cout << "Fitting..." << std::endl;
+	HNSW<double, L2Distance> alg(/*M*/ 16, /*efConstruction*/ 100, /*efSearch*/ 100);
+	alg.fit(data);
+
+	std::cout << "Searching..." << std::endl;
+	std::default_random_engine gen;
+	std::uniform_int_distribution<size_t> dis{ 0, data.size() - 1 };
+	const size_t n = 100;
+	for (size_t i = 0; i < n; i++)
+	{
+		size_t query_index = dis(gen);
+		std::vector<double> query = data[query_index];
+		size_t top_n = 10;
+
+		auto result = alg.knn_query(query, top_n);
+		EXPECT_EQ(data[result.front()], data[query_index]);
+		size_t count = std::count(result.begin(), result.end(), query_index);
+		EXPECT_EQ(count, 1);
+		EXPECT_EQ(result.size(), top_n);  // make sure we always take enough neighbors
+	}
+}
